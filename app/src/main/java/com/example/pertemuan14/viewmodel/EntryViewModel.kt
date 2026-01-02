82c42b7096c853668d689ab29ab4efee1e2299c2
mauraaninditakirana <kiranamaura351@gmail.com>
2026-01-02 13:59:34 +0700

    Membuat EntryViewModel untuk memvalidasi dan menyimpan data Siswa.

---
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.pertemuan14.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan14.modeldata.DetailSiswa
+import com.example.pertemuan14.modeldata.UIStateSiswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+import com.example.pertemuan14.modeldata.toDataSiswa
+import androidx.compose.runtime.*
+
+class EntryViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean
+    {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            repositorySiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+        }
+    }
+}
